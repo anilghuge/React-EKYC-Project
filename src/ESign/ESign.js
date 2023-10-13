5c2d20b051c929b9488d1c4266409b52da3ac885
dnyanenagapure <110399783+dnyanenagapure@users.noreply.github.com>
2023-10-13 12:08:06 +0530

    Added ESign checkboxes validation

--- src/ESign/ESign.js
+++ src/ESign/ESign.js
@@ -1,59 +1,109 @@
 import React, { useState } from "react";
-import { Container, Typography, Checkbox, FormControlLabel, LinearProgress, Grid, Button } from '@mui/material';
+import {
+  Container,
+  Typography,
+  Checkbox,
+  FormControlLabel,
+  LinearProgress,
+  Grid,
+  Button,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 const ESignPage = () => {
-    const navigate = useNavigate();
-    const [progress, setProgress] = useState(0);
+  const navigate = useNavigate();
+  const [progress, setProgress] = useState(0);
+  const [consentAadhaar, setConsentAadhaar] = useState(false);
+  const [consentTAndC, setConsentTAndC] = useState(false);
+  const [error, setError] = useState("");
 
-    const handleCheckboxChange = (event) => {
-        // Update the progress based on the number of checked checkboxes
-        const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-        const checkedCheckboxes = [...checkboxes].filter(checkbox => checkbox.checked);
-        setProgress((checkedCheckboxes.length / checkboxes.length) * 100);
-    };
+  const handleCheckboxChange = (event) => {
+    if (event.target.name === "consentAadhaar") {
+      setConsentAadhaar(event.target.checked);
+    } else if (event.target.name === "consentTAndC") {
+      setConsentTAndC(event.target.checked);
+    }
+  };
 
-    const handleProceedClick = () => {
-        navigate("/congratulation");
-    };
+  const handleProceedClick = () => {
+   if (!consentAadhaar) {
+      setError("Please select the 'Consent for e-sign using Aadhaar OTP' checkbox.");
+    } else if (!consentTAndC) {
+      setError("Please select the 'Consent for T&C' checkbox.");
+    } else {
+      setError("");
+      setProgress(100);
+      navigate("/congratulation");
+    }
+  };
 
-    return (
-        <Container>
-            <Grid  container
-            spacing={2}
-            justifyContent="center"
-            alignItems="center"
-            style={{ minHeight: "50vh" }}>
-                <Grid item xs={12} sm={6}>
-                    <Typography variant="h6" gutterBottom>
-                        E-sign Document: {Math.round(progress)}%
-                    </Typography>
-                    <LinearProgress variant="determinate" value={progress} sx={{ width: "70%", height: "12px" }} />
+  return (
+    <Container>
+      <Grid
+        container
+        spacing={2}
+        justifyContent="center"
+        alignItems="center"
+        style={{ minHeight: "50vh" }}
+      >
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h6" gutterBottom>
+            E-sign Document: {Math.round(progress)}%
+          </Typography>
+          <LinearProgress
+            variant="determinate"
+            value={progress}
+            sx={{ width: "70%", height: "12px" }}
+          />
 
-                    <FormControlLabel
-                        control={<Checkbox onChange={handleCheckboxChange} />}
-                        label="Consent for e-sign using Aadhaar OTP"
-                    />
+          <FormControlLabel
+            control={
+              <Checkbox
+                name="consentAadhaar"
+                checked={consentAadhaar}
+                onChange={handleCheckboxChange}
+              />
+            }
+            label="Consent for e-sign using Aadhaar OTP"
+          />
 
-                    <FormControlLabel
-                        control={<Checkbox onChange={handleCheckboxChange} />}
-                        label="Consent for T&C"
-                    />
-                    <Button variant="contained" color="primary" fullWidth onClick={handleProceedClick}>
-                        Proceed to E-Sign
-                    </Button>
-                </Grid>
+          <FormControlLabel
+            control={
+              <Checkbox
+                name="consentTAndC"
+                checked={consentTAndC}
+                onChange={handleCheckboxChange}
+              />
+            }
+            label="Consent for T&C"
+          />
+          {error && (
+            <div style={{ color: "red" }}>
+              {error}
+            </div>
+          )}
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={handleProceedClick}
+          >
+            Proceed to E-Sign
+          </Button>
+        </Grid>
 
-                <Grid item xs={12} sm={6}>
-                    <Typography variant="h6">What is E-sign?</Typography>
-                    <Typography>
-                        eSign is an online service for electronic signatures without using physical cryptographic token.
-                        Application service providers use e-KYC service to authenticate signers and facilitate digital signing of documents.
-                    </Typography>
-                </Grid>
-            </Grid>
-        </Container>
-    );
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h6">What is E-sign?</Typography>
+          <Typography>
+            eSign is an online service for electronic signatures without using
+            physical cryptographic token. Application service providers use e-KYC
+            service to authenticate signers and facilitate digital signing of
+            documents.
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default ESignPage;
